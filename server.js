9fa28d50fa2c88d38e42fe2a1d9acb9797968cb1
Lady Stock <nikki.castlea@gmail.com>
2020-07-27 18:41:43 -0600

    switch case for initial prompt written

--- server.js
+++ server.js
@@ -10,4 +10,44 @@ const connection = mysql.createConnection({
     database: "employee_db"
 });
 
-connection.connect();
+connection.connect((err) => {
+    if (err) throw err;
+    start();
+});
+
+const start = () => {
+    inquirer.prompt({
+            type: "list",
+            name: "method",
+            message: "What would you like to do?",
+            choices: ["Add Employee", "Update Employee Role", "View All Employees", "Add Department", "View All Departments", "Add Role", "View All Roles"]
+        })
+        .then((answer) => {
+            switch (method) {
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployee();
+                    break;
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                default:
+                    console.log("You must select one of the above.");
+
+
+            }
+        })
+}
