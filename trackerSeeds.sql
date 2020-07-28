5f808794579662a2bf9b5dd0d2ff14c69562f74a
Lady Stock <nikki.castlea@gmail.com>
2020-07-28 14:25:10 -0600

    updated char length on VARCHARs

--- trackerSeeds.sql
+++ trackerSeeds.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL;
-    title VARCHAR(50) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     INDEX dep_ind (department_id),
@@ -19,7 +19,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
+    manager_id INT
 )
